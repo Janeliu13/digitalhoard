3bb838b8f70019bffbd70d1f86c3b09790268ef9
Janeliu13 <Jazliu09@gmail.com>
2025-02-24 10:12:05 +0800

    commit all files

---
+++ readFileName.py
@@ -0,0 +1,75 @@
+import os
+import re
+
+def clean_filename(filename):
+    # 删除乱码字符：匹配所有非 ASCII 字符并删除
+    filename = re.sub(r'[^\x00-\x7F]+', '', filename)
+    
+    # 替换空格和@符号为下划线
+    filename = filename.replace(" ", "_").replace("@", "_").replace("#", "_")
+    
+    return filename
+
+def organize_files_by_extension(directory_path):
+    # 初始化存储不同类型文件的数组
+    jpg_files = []
+    png_files = []
+    gif_files = []
+    other_files = []
+
+    # 获取指定文件夹下的所有文件
+    for filename in os.listdir(directory_path):
+        # 获取文件的完整路径
+        file_path = os.path.join(directory_path, filename)
+
+        # 仅处理文件而非文件夹
+        if os.path.isfile(file_path):
+            # 清理文件名
+            cleaned_filename = clean_filename(filename)
+            
+            # 重命名文件
+            if cleaned_filename != filename:
+                os.rename(file_path, os.path.join(directory_path, cleaned_filename))
+                filename = cleaned_filename  # 更新文件名
+
+            # 获取文件扩展名（小写）
+            file_extension = filename.split('.')[-1].lower()
+
+            # 根据文件扩展名将文件名添加到相应的数组
+            if file_extension == 'jpg':
+                jpg_files.append(filename)
+            elif file_extension == 'png':
+                png_files.append(filename)
+            elif file_extension == 'gif':
+                gif_files.append(filename)
+            else:
+                other_files.append(filename)
+
+    return jpg_files, png_files, gif_files, other_files
+
+
+def save_to_txt(jpg_files, png_files, gif_files, other_files, output_path):
+    with open(output_path, 'w') as f:
+        # 写入每个类型的文件名到文本文件，以数组格式存储
+        f.write(f'JPG Files: {str(jpg_files)}\n')
+        f.write(f'PNG Files: {str(png_files)}\n')
+        f.write(f'GIF Files: {str(gif_files)}\n')
+        f.write(f'Other Files: {str(other_files)}\n')
+
+
+def main():
+    # 设置文件夹路径和输出文件路径
+    directory_path = 'assets'  # 更改为目标文件夹路径
+    output_path = 'output_files.txt'  # 输出结果的文本文件路径
+
+    # 组织文件
+    jpg_files, png_files, gif_files, other_files = organize_files_by_extension(directory_path)
+
+    # 将文件名保存到文本文件
+    save_to_txt(jpg_files, png_files, gif_files, other_files, output_path)
+
+    print(f"文件已保存至 {output_path}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ sketch.js
@@ -0,0 +1,139 @@
+let imgFileNames = ['$26.00.jpg', '2YardsOfOrganza.jpg', '3620fujifilm.jpg', '3DScanRoom.jpg', 'acceptances.jpg', 'accountSign-ins.jpg', 'ahhhh.jpg', 'aliencat.jpg', 'ALL1N_Wallpaper.jpg', 'allAPPpasswords.jpg', 'allAPPpasswords2.jpg', 'angelaonthebigscreen.jpg', 'apartmentsNyc.jpg', 'BajaBlast.jpg', 'bananapile.jpg', 'BarneyGallery.jpg', 'bass.jpg', 'bathroombreak.jpg', 'bathroomPoster.jpg', 'bigwin.jpg', 'BM3730_sewing_machine.jpg', 'bottles.jpg', 'break.jpg', 'bug.jpg', 'casestudy.jpg', 'casio.jpg', 'CatGoneMissing.jpg', 'ccd020725.jpg', 'chanchan.jpg', 'ChinaVisaApp.jpg', 'chsID.jpg', 'class7score_85_.jpg', 'commonAppPlan.jpg', 'CopyOfaCopyOfaCopy.jpg', 'cosine_wave.jpg', 'cosmicsea.jpg', 'covidtest3132022.jpg', 'cvsCheckout.jpg', 'dearX.jpg', 'demure.jpg', 'deskspread.jpg', 'deskspread2.jpg', 'doge.jpg', 'egg.jpg', 'ego.jpg', 'emailtoevan.jpg', 'energyinteraction.jpg', 'EtymologyofArctic.jpg', 'excerptfrom.jpg', 'Fairmont.jpg', 'feelingblue.jpg', 'figuredrawing.jpg', 'figures2.jpg', 'fillthesejars.jpg', 'FinalPrez.jpg', 'floating.jpg', 'Frankie&straws.jpg', 'fruitstickers.jpg', 'Gangnum-Gu_trash.jpg', 'garbagedress2.jpg', 'getondiscord.jpg', 'glitch.jpg', 'glitch2.jpg', 'googlesearch.jpg', 'googletranslate.jpg', 'govofcanada.jpg', 'graffitiRoom.jpg', 'graffitiRoom2.jpg', 'granvilleisland.jpg', 'GST_registration_.jpg', 'hbdSky.jpg', 'hoard1.jpg', 'hoard2.jpg', 'hoard3.jpg', 'hoard4.jpg', 'hoard5.jpg', 'hoard6.jpg', 'hoard7.jpg', 'hoard8.jpg', 'holographic.jpg', 'hotsummer.jpg', 'icloudpassword2020.jpg', 'IMG_1652.jpg', 'IMG_1857.jpg', 'IMG_2388.jpg', 'IMG_3351.jpg', 'IMG_3445.jpg', 'IMG_3909.jpg', 'IMG_4187.jpg', 'IMG_5122.jpg', 'IMG_5241.jpg', 'IMG_5729.jpg', 'IMG_5767.jpg', 'IMG_5919.jpg', 'IMG_6392.jpg', 'IMG_6393.jpg', 'IMG_6691.jpg', 'IMG_6738.jpg', 'IMG_6739.jpg', 'IMG_6816.jpg', 'IMG_6997.jpg', 'IMG_7194.jpg', 'IMG_7197.jpg', 'IMG_7213.jpg', 'IMG_7227.jpg', 'IMG_7391.jpg', 'IMG_8292.jpg', 'IMG_8886.jpg', 'IMG_8887.jpg', 'IMG_8945.jpg', 'IMG_9008.jpg', 'IMG_9021.jpg', 'IMG_9051.jpg', 'IMG_9260.jpg', 'IMG_9689.jpg', 'IMG_9969.jpg', 'importantDoc2021.jpg', 'ineedsome.jpg', 'inkonshirt.jpg', 'insta_poll.jpg', 'instructions.jpg', 'Ishoulddosomething.jpg', 'jojosiwaCereal.jpg', 'keepOUT.jpg', 'kitty.jpg', 'lakers.jpg', 'LAX5dayplan.jpg', 'leftover.jpg', 'legs.jpg', 'letterTo_.jpg', 'LicenceOLD.jpg', 'lighter.jpg', 'lindatheworm.jpg', 'lizardskin.jpg', 'lol.jpg', 'MacSailing.jpg', 'Marchschedule.jpg', 'me&angela.jpg', 'memoresofMatsuko2.jpg', 'memoriesofMatsuko.jpg', 'memoriesofMatsuko3.jpg', 'memoriesofMatsuko4.jpg', 'memoriesofMatusko5.jpg', 'mindmap.jpg', 'mirrorOnmirrorONmirror.jpg', 'mirrorpic.jpg', 'mirrorselfie.jpg', 'mirrorselfie2.jpg', 'morningslikethis.jpg', 'mother1986.jpg', 'mo_An.jpg', 'myCube.jpg', 'nakedMoleRat.jpg', 'needs2beHumbled.jpg', 'newMAC.jpg', 'newyears&Dog.jpg', 'nightview.jpg', 'oldTV.jpg', 'oldTV2.jpg', 'omeglechat.jpg', "omwToTinaLee's.jpg", 'onFilm.jpg', 'onFilm2.jpg', 'password_dcclxviii.jpg', 'pastelonpaper.jpg', 'pastelonpaper2.jpg', 'PC11_99_.jpg', 'photoday2022.jpg', 'pink_tutu.jpg', 'poetry.jpg', 'polaroid2022.jpg', 'PortaPotty.jpg', 'postedbyiphone5.jpg', 'postedbyScarlett.jpg', 'preschoolGal.jpg', 'printedpapers.jpg', 'PSlightningGal.jpg', 'reportcard.jpg', 'Screenshot_2019-09-23_at_06.19.32M.jpg', 'secretrecoveryphrase.jpg', 'securitycam.jpg', 'selfie.jpg', 'selfieSky.jpg', 'sentfromMaiko.jpg', 'sheep.jpg', 'sheep3.jpg', 'sheplaystheguitar.jpg', 'sittingmeditation.jpg', 'sketchbook.jpg', 'sketchbook2.jpg', 'sketchbook4.jpg', 'sky07232011.jpg', 'slideroom.jpg', 'slurp.jpg', 'smartSEOregister.jpg', 'snoozing.jpg', 'somecityscape.jpg', 'SpanishConjugations.jpg', 'splatters.jpg', 'splatters2.jpg', 'spread2.jpg', 'squarehairsalon.jpg', 'storeinKor.jpg']
+let bg
+let imgFiles = []
+let draggableImgs = []
+let dragging = false
+let dClick = false
+let offsetX = -1, offsetY = -1
+let selectIndex = -1;
+let openIndex = -1
+let randX,randY
+function preload() {
+  for (let i = 0; i < imgFileNames.length; i++) {
+    let img = loadImage("assets/" + imgFileNames[i])
+    // img.resize(50,0)
+    imgFiles.push(img)
+
+  }
+  bg = loadImage("assets/bg.jpg")
+
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER)
+  image(bg, width / 2, height / 2, width, height)
+  for (let i = 0; i < imgFiles.length; i++) {
+    let dImg = new DraggableImg(imgFiles[i], imgFileNames[i])
+    draggableImgs.push(dImg)
+  }
+
+
+}
+function mouseClicked() {
+  console.log((mouseX / width).toFixed(2), (mouseY / height).toFixed(2))
+}
+function draw() {
+  image(bg, width / 2, height / 2, width, height)
+
+  // background(220);
+  for (let i = 0; i < draggableImgs.length; i++) {
+    draggableImgs[i].display()
+  }
+
+
+  if (selectIndex > -1) {
+    dragImg = draggableImgs[selectIndex]
+    dragImg.display()
+
+    dragImg.pos.x = mouseX - offsetX
+    dragImg.pos.y = mouseY - offsetY
+  } else {
+
+    for (let i = draggableImgs.length - 1; i >= 0; i--) {
+      let dragImg = draggableImgs[i]
+      if (dragging == true) {
+        if (dragImg.inArea() == true) {
+          if (offsetX == -1 && offsetY == -1) {
+            offsetX = mouseX - dragImg.pos.x
+            offsetY = mouseY - dragImg.pos.y
+            selectIndex = i
+          }
+
+          break;
+        }
+      }
+      if(dClick==true){
+        if (dragImg.inArea() == true) {
+           openIndex = i
+           let w = imgFiles[openIndex].width /imgFiles[openIndex].height*height*0.7 
+           let h = height*0.6
+           randX = random(w/2,width-w/2)
+           randY = random(h/2+height * 0.04,height*0.94-h/2)
+           break;
+        }
+      }
+    }
+
+  }
+
+  if(openIndex>-1){
+    let w = imgFiles[openIndex].width /imgFiles[openIndex].height*height*0.7 
+    let h = height*0.6
+    image(imgFiles[openIndex],randX,randY,w,h)
+
+    if(mouseIsPressed){
+      let w = imgFiles[openIndex].width/imgFiles[openIndex].height*height*0.7 
+    let h = height*0.6
+      if(abs(mouseX-randX)<w/2 && abs(mouseY-randY)<h/2){
+        openIndex = -1
+      }
+    }
+  }
+  // }
+    
+  
+  dClick = false
+
+}
+
+
+class DraggableImg {
+  constructor(img, name) {
+    this.img = img
+    this.name = name
+    this.pos = createVector(random(75 / 2, width - 75 / 2), random(height * 0.04 + 75 / 2, height * 0.94 - 75))
+    this.w = random([50, 75])
+    this.h = this.img.height / this.img.width * 50
+  }
+  display() {
+    image(this.img, this.pos.x, this.pos.y, this.w, this.h)
+    fill(255)
+    textSize(15)
+    textAlign(CENTER)
+    text(this.name, this.pos.x, this.pos.y + this.h / 2 + 20)
+  }
+  inArea() {
+    // if(dragging){
+    if (abs(mouseX - this.pos.x) < this.w / 2 && abs(mouseY - this.pos.y) < this.h / 2) {
+      return true
+    }
+    // }
+    return false
+
+  }
+}
+
+function mouseDragged() {
+  dragging = true
+}
+function mouseReleased() {
+  dragging = false
+  selectIndex = -1
+  offsetX = -1
+  offsetY = -1
+
+}
+function doubleClicked() {
+  dClick = true
+  console.log("dclick", dClick)
+}
